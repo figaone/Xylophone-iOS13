a4dc2690bf1070ac48ac3fba610d25868dcf4547
kojo Adu-Gyamfi <adugyamfikonadu48@gmail.com>
2024-07-02 21:47:32 -0500

    add dynamic sound playing

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -11,33 +11,34 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
-    var player: AVAudioPlayer?
-
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
+    
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound()
+        playSound(soundName: sender.currentTitle!)
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+        
     }
     
     
-    func playSound() {
-            guard let path = Bundle.main.path(forResource: "C", ofType: "wav") else {
-                print("Sound file not found")
-                return
-            }
+    func playSound(soundName: String) {
+            let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
             
-            let url = URL(fileURLWithPath: path)
-            
-            do {
-                player = try AVAudioPlayer(contentsOf: url)
-                player?.play()
-            } catch {
-                print("Error playing sound: \(error.localizedDescription)")
-            }
         }
     
-
+    
 }
